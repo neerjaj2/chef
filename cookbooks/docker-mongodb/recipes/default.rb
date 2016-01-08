58db45082daa86a38adde739e17bcb8fb789d0e8
neerjaj2 <neerjaj2@gmail.com>
2016-01-08 15:58:30 +0530

    Update default.rb

--- cookbooks/docker-mongodb/recipes/default.rb
+++ cookbooks/docker-mongodb/recipes/default.rb
@@ -6,31 +6,38 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-
 docker_service 'default' do
-        action [:create, :start]
+	action [:create, :start]
 end
 
-
-docker_image 'neerjaj2/mongodb-docker' do
-        tag 'v0.1.0'
-        source '/home/ubuntu/chef-repo/cookbooks/docker-mongodb/files/default/Dockerfile'
-        action :build_if_missing
+docker_image 'neerjaj2/mongodb' do
+	action :pull
 end
-
-
-
-
-"docker_image 'neerjaj2/mongodb' do
-        action :pull
-end"
 
 docker_container 'launch' do
-        repo 'neerjaj2/mongodb-docker'
-        tag 'v0.1.0'
-        hostname 'mongo1'
-        domain_name 'ttnd.com'
-        publish_all_ports
-        command '--replSet ttnd --noprealloc --smallfiles'
+	repo 'neerjaj2/mongodb'
+	tag 'latest'
+	hostname 'mongo1'
+	domain_name 'ttnd.com'
+	publish_all_ports 
+	command '--replSet ttnd --noprealloc --smallfiles'
 end
 
+
+docker_container 'launch2' do
+	repo 'neerjaj2/mongodb'
+	tag 'latest'
+	hostname 'mongo2'
+	domain_name 'ttnd.com'
+	publish_all_ports
+	command '--replSet ttnd --noprealloc --smallfiles'
+end
+
+docker_container 'launch3' do
+	repo 'neerjaj2/mongodb'
+	tag 'latest'
+	hostname 'mongo3'
+	domain_name 'ttnd.com'
+	publish_all_ports
+	command '--replSet ttnd --noprealloc --smallfiles'
+end
